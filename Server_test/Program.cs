bc59951d75bfd9a532ac0e2a5be6b7e1d18b4061
mazurjakub <71710141+mazurjakub@users.noreply.github.com>
2022-07-20 20:05:54 +0200

    fd

--- Server_test/Program.cs
+++ Server_test/Program.cs
@@ -9,38 +9,46 @@ namespace Server_test
     {
         static void Main(string[] args)
         {
-            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1700);
-            listener.Start();
-            while (true)
+
+            try
             {
-                Console.WriteLine("Čekám na spojení.");
-                TcpClient client = listener.AcceptTcpClient();
-                Console.WriteLine("Klient přijat." + client.ToString());
-                NetworkStream stream = client.GetStream();
-                StreamReader sr = new StreamReader(client.GetStream());
-                StreamWriter sw = new StreamWriter(client.GetStream());
-                try
+                TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1700);
+                listener.Start();
+                while (true)
                 {
-                    byte[] buffer = new byte[1024];
-                    stream.Read(buffer, 0, buffer.Length);
-                    int recv = 0;
-                    foreach (byte b in buffer)
+                    Console.WriteLine("Čekám na spojení.");
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("Klient přijat." + client.ToString());
+                    NetworkStream stream = client.GetStream();
+                    StreamReader sr = new StreamReader(client.GetStream());
+                    StreamWriter sw = new StreamWriter(client.GetStream());
+                    try
                     {
-                        if (b != 0)
+                        byte[] buffer = new byte[1024];
+                        stream.Read(buffer, 0, buffer.Length);
+                        int recv = 0;
+                        foreach (byte b in buffer)
                         {
-                            recv++;
+                            if (b != 0)
+                            {
+                                recv++;
+                            }
                         }
+                        string request = Encoding.UTF8.GetString(buffer, 0, recv);
+                        Console.WriteLine("Žádost klienta přijata: " + request);
+                        sw.WriteLine("Žádost úspěšně přijata.\n");
+                        sw.Flush();
                     }
-                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
-                    Console.WriteLine("Žádost klienta přijata: " + request);
-                    sw.WriteLine("Žádost úspěšně přijata.\n");
-                    sw.Flush();
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Něco se nepovedlo.");
+                        sw.WriteLine(e.ToString());
+                    }
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Něco se nepovedlo.");
-                    sw.WriteLine(e.ToString());
-                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
